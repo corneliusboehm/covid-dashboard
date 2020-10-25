ab88b347ffe89223df13e369ad639cf0c12f641e
crekowski <cornelius.vonrekowski@twentybn.com>
2020-10-25 03:37:47 +0100

    Add columns for new and new relative cases

--- js/table.js
+++ js/table.js
@@ -7,7 +7,13 @@ const columns = {
     CONFIRMED_RELATIVE: 5,
     DEATHS_RELATIVE: 6,
     RECOVERED_RELATIVE: 7,
-    SELECTED: 8,
+    CONFIRMED_INCREASE: 8,
+    DEATHS_INCREASE: 9,
+    RECOVERED_INCREASE: 10,
+    CONFIRMED_INCREASE_RELATIVE: 11,
+    DEATHS_INCREASE_RELATIVE: 12,
+    RECOVERED_INCREASE_RELATIVE: 13,
+    SELECTED: 14,
 }
 
 let selectedCountries = [
@@ -40,11 +46,27 @@ $(document).ready( function () {
                 columns.CONFIRMED_RELATIVE,
                 columns.DEATHS_RELATIVE,
                 columns.RECOVERED_RELATIVE,
+                columns.CONFIRMED_INCREASE,
+                columns.DEATHS_INCREASE,
+                columns.RECOVERED_INCREASE,
+                columns.CONFIRMED_INCREASE_RELATIVE,
+                columns.DEATHS_INCREASE_RELATIVE,
+                columns.RECOVERED_INCREASE_RELATIVE,
                 columns.SELECTED,
             ]},
             {'visible': false, 'targets': [
+                // columns.CONFIRMED_TOTAL,
+                // columns.DEATHS_TOTAL,
                 columns.RECOVERED_TOTAL,
+                // columns.CONFIRMED_RELATIVE,
+                columns.DEATHS_RELATIVE,
                 columns.RECOVERED_RELATIVE,
+                columns.CONFIRMED_INCREASE,
+                columns.DEATHS_INCREASE,
+                columns.RECOVERED_INCREASE,
+                // columns.CONFIRMED_INCREASE_RELATIVE,
+                columns.DEATHS_INCREASE_RELATIVE,
+                columns.RECOVERED_INCREASE_RELATIVE,
                 columns.SELECTED,
             ]}
         ],
@@ -57,6 +79,12 @@ $(document).ready( function () {
             {orderSequence: ['desc', 'asc']},  // CONFIRMED_RELATIVE
             {orderSequence: ['desc', 'asc']},  // DEATHS_RELATIVE
             {orderSequence: ['desc', 'asc']},  // RECOVERED_RELATIVE
+            {orderSequence: ['desc', 'asc']},  // CONFIRMED_INCREASE
+            {orderSequence: ['desc', 'asc']},  // DEATHS_INCREASE
+            {orderSequence: ['desc', 'asc']},  // RECOVERED_INCREASE
+            {orderSequence: ['desc', 'asc']},  // CONFIRMED_INCREASE_RELATIVE
+            {orderSequence: ['desc', 'asc']},  // DEATHS_INCREASE_RELATIVE
+            {orderSequence: ['desc', 'asc']},  // RECOVERED_INCREASE_RELATIVE
             null,  // SELECTED
         ],
         buttons: [
@@ -98,44 +126,64 @@ $(document).ready( function () {
 
 function updateTableData() {
     let lastDate = data.deaths.dates[data.deaths.dates.length - 1];
+    let secondToLastDate = data.deaths.dates[data.deaths.dates.length - 2];
+
     for (const row of data.deaths.data) {
         let country = row[COUNTRY_KEY];
+        
+        // Population
         let pop = getPopulation(country);
         let pop100k = pop != null ? pop / 100000 : null
 
+        // Flag
         let flagURL = getFlag(country);
         let flag = flagURL != null ? '<img src="' + flagURL + '" class="flag">' : null;
 
+        // Deaths
         let deaths = row[lastDate];
-        let deathsRelative = '';
+        let deathsIncrease = deaths - row[secondToLastDate];
+        let deathsRelative = 0;
+        let deathsIncreaseRelative = 0;
         if (pop100k != null) {
             deathsRelative = Math.round(deaths / pop100k);
+            deathsIncreaseRelative = Math.round(deathsIncrease / pop100k);
         }
 
-        let confirmed = getCountryData(country, 'confirmed', 'total', false, false);
-        let confirmedRelative = '';
-        if (confirmed != null) {
-            confirmed = confirmed[confirmed.length - 1];
+        // Confirmed
+        let confirmedRow = getCountryData(country, 'confirmed', 'total', false, false);
+        let confirmed = 0;
+        let confirmedIncrease = 0;
+        let confirmedRelative = 0;
+        let confirmedIncreaseRelative = 0;
+        if (confirmedRow != null) {
+            confirmed = confirmedRow[confirmedRow.length - 1];
+            confirmedIncrease = confirmed - confirmedRow[confirmedRow.length - 2];
 
             if (pop100k != null) {
                 confirmedRelative = Math.round(confirmed / pop100k);
+                confirmedIncreaseRelative = Math.round(confirmedIncrease / pop100k);
             }
-        } else {
-            confirmed = 0;
         }
 
-        let recovered = getCountryData(country, 'recovered', 'total', false, false);
-        let recoveredRelative = ''
-        if (recovered != null) {
-            recovered = recovered[recovered.length - 1];
+        // Recovered
+        let recoveredRow = getCountryData(country, 'recovered', 'total', false, false);
+        let recovered = 0;
+        let recoveredIncrease = 0;
+        let recoveredRelative = 0;
+        let recoveredIncreaseRelative = 0;
+        if (recoveredRow != null) {
+            recovered = recoveredRow[recoveredRow.length - 1];
+            recoveredIncrease = recovered - recoveredRow[recoveredRow.length - 2];
 
             if (pop100k != null) {
                 recoveredRelative = Math.round(recovered / pop100k);
+                recoveredIncreaseRelative = Math.round(recoveredIncrease / pop100k);
             }
         } else {
             recovered = 0;
         }
 
+        // Add row
         rowNode = table.row.add([
             flag,
             country,
@@ -145,6 +193,12 @@ function updateTableData() {
             confirmedRelative.toLocaleString('en-US'),
             deathsRelative.toLocaleString('en-US'),
             recoveredRelative.toLocaleString('en-US'),
+            confirmedIncrease.toLocaleString('en-US'),
+            deathsIncrease.toLocaleString('en-US'),
+            recoveredIncrease.toLocaleString('en-US'),
+            confirmedIncreaseRelative.toLocaleString('en-US'),
+            deathsIncreaseRelative.toLocaleString('en-US'),
+            recoveredIncreaseRelative.toLocaleString('en-US'),
             0
         ]).node();
     }
